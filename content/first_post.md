c3b12b3c19fe7e1bf2dafdfef31f123e989fd3a6
virtuoso-rotins <>
2021-07-31 19:16:30 +0200

    first commit

---
+++ content/first_post.md
@@ -0,0 +1,11 @@
+Title: My first post
+Date: 2021-07-21 18:00
+Modified: 2021-07-21 18:49
+Category: misc
+Tags: first, misc
+Slug: My-fist-post
+Authors: rotins
+Summary: This is a test to see how the deployment is from Pellican to Github Pages.
+
+This is the *first post* from my Pelixan blog. **YAY!**
+![This is only a icon]({static}/images/icon.png)
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Rotins'
+SITENAME = 'The blog of Virtuoso Rotins'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['images']
+
+TIMEZONE = 'Europe/Madrid'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Change to this later
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# FEED_ALL_RSS = 'feeds/all.rss.xml'
+# AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+# RSS_FEED_SUMMARY_ONLY = False
+
+
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
